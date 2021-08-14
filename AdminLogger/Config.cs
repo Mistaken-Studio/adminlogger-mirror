e196ee577e1f6819f0d5ece8ca34ead0591031d1
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-08-14 13:12:19 +0200

    Changed default AutoUpdateConfig value

--- AdminLogger/Config.cs
+++ AdminLogger/Config.cs
@@ -17,6 +17,12 @@ namespace Mistaken.AdminLogger
 
         /// <inheritdoc/>
         [Description("Auto Update Settings")]
-        public System.Collections.Generic.Dictionary<string, string> AutoUpdateConfig { get; set; }
+        public System.Collections.Generic.Dictionary<string, string> AutoUpdateConfig { get; set; } = new System.Collections.Generic.Dictionary<string, string>
+        {
+            { "Url", "https://git.mistaken.pl/api/v4/projects/28" },
+            { "Token", string.Empty },
+            { "Type", "GITLAB" },
+            { "VerbouseOutput", "false" },
+        };
     }
 }
